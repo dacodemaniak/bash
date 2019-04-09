7d7b1a58f5dd215df653b5e0e7dd42ba7505c392
Jean-Luc Aubert <jean-luc.a@web-projet.com>
2019-04-09 08:15:08 +0200

    Contrôle de l'existence du dossier

--- addVhost.sh
+++ addVhost.sh
@@ -9,33 +9,43 @@
 sudo -v
 
 # Définit le dossier racine des applications Apache
-ROOT_DIR="/home/jean-luc/www/"
+DOCUMENT_ROOT="/home/jean-luc/www/"
 
 if [ $# == 0 ]
 then
 	echo "To few arguments to process. Operation failed"
 	exit -1
 else
-	# Copier le fichier template-symfony vers /etc/apache2/sites-available
-	# avec le nom vhost_name.conf
-	cp ./template-symfony /etc/apache2/sites-available/$1.conf
+	# Stocker le dossier dans une variable
+	DIRECTORY=$1
 
-	# Créer le dossier dans le dossier /home/jlaubert/www
-	mkdir -p $ROOT_DIR$1 $ROOT_DIR$1/web
+	# Tester l'existence du dossier
+	if [! -d "$DOCUMENT_ROOT$DIRECTORY" ]
+	then
+		# Copier le fichier template-symfony vers /etc/apache2/sites-available
+		# avec le nom vhost_name.conf
+		cp ./template-symfony /etc/apache2/sites-available/$1.conf
 
-	# Remplacer "template" par "vhost_name" dans le fichier de configuration
-	sudo sed -i 's/template/'$1'/g' /etc/apache2/sites-available/$1.conf
+		# Créer le dossier dans le dossier /home/jlaubert/www
+		mkdir -p $DOCUMENT_ROOT$1 $DOCUMENT_ROOT$1/web
 
-	# Ajouter le fichier de configuration à la liste des sites actifs
-	sudo a2ensite $1.conf
+		# Remplacer "template" par "vhost_name" dans le fichier de configuration
+		sudo sed -i 's/template/'$1'/g' /etc/apache2/sites-available/$1.conf
 
-	# Mettre à jour le fichier hosts
-	echo "127.0.0.1 $1.wrk www.$1.wrk" >> /etc/hosts
+		# Ajouter le fichier de configuration à la liste des sites actifs
+		sudo a2ensite $1.conf
 
-	# Vérifier l'exécution globale
-	sudo touch $ROOT_DIR$1/web/app.php
-	echo "<?php phpinfo();" >> $ROOT_DIR$1/web/app.php
+		# Mettre à jour le fichier hosts
+		echo "127.0.0.1 $1.wrk www.$1.wrk" >> /etc/hosts
 
-	# Relancer Apache
-	sudo service apache2 reload
+		# Vérifier l'exécution globale
+		sudo touch $DOCUMENT_ROOT$1/web/app.php
+		echo "<?php phpinfo();" >> $DOCUMENT_ROOT$1/web/app.php
+
+		# Relancer Apache
+		sudo service apache2 reload
+	else
+		echo "Le dossier $DOUCMENT_ROOT$DIRECTORY a déjà été créé."
+		exit -1
+	fi
 fi
